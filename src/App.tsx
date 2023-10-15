4a57a360c28c0650f38eaf38fb805e87054d3cc3
Ryan Wiesenberg <rmwiesenberg@gmail.com>
2023-10-15 14:34:55 -0500

    Add initial deck gl implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeckGL from '@deck.gl/react/typed';
+import {TileLayer} from '@deck.gl/geo-layers/typed';
+import {BitmapLayer} from '@deck.gl/layers/typed';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const layer = new TileLayer({
+    id: 'TileLayer',
+    data: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+
+    maxZoom: 19,
+    minZoom: 0,
+
+    renderSubLayers: props => {
+      const {
+        // @ts-ignore
+        bbox: {west, south, east, north}
+      } = props.tile;
+
+      return new BitmapLayer(props, {
+        data: undefined,
+        image: props.data,
+        bounds: [west, south, east, north]
+      });
+    },
+  });
+
+  return <DeckGL initialViewState={{latitude:40.7, longitude:-74, zoom:11}} layers={[layer]} />;
 }
 
 export default App;
